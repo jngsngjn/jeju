c5148fed7b054ca6f889d268a777053095cab4b3
ohmintaeak <omt610@gmail.com>
2024-05-21 19:44:20 +0900

    네이버버튼완성

--- src/main/front/src/App.js
+++ src/main/front/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from "axios";
+import './App.css';
+import naverLogo from './naver_logo.png';
 
 function selectData() {
   axios.get('/hello')  // 프록시 설정에 의해 http://localhost:8080이 자동으로 붙습니다.
@@ -12,8 +14,18 @@ function selectData() {
 }
 
 function App() {
+  const handleLogin = () => {
+      window.location.href = 'https://nid.naver.com/nidlogin.login';
+  };
+
   return (
-      <button onClick={selectData}>test</button>
+      <div className="App">
+          <header className="App-header">
+              <button onClick={handleLogin} className="naver-button">
+                  <img src={naverLogo} alt="Naver Logo" className="naver-logo" />
+              </button>
+          </header>
+      </div>
   );
 }
 
