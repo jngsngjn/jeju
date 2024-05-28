5f9a9ecb13a0b46240f2984ab701d0d471104295
ohmintaeak <omt610@gmail.com>
2024-05-28 20:38:11 +0900

    page routing2

--- src/main/front/src/home.jsx
+++ src/main/front/src/home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import {useNavigate} from "react-router-dom";
+import jeju_main_1 from "./image/jeju_main_1.png";
+import './Home.css';
 
 const Home = (props) => {
     const navigate = useNavigate();
     return (
-        <h1>Home</h1>
+        <div>
+            <img src={jeju_main_1} className="jeju-main-1" />
+            <h3>카테고리</h3>
+        </div>
     );
 };
 
